709a01029f21ebb60431d933821dcd442bed6c64
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-08-03 16:58:54 +0300

    правки

--- task/Parser.cpp
+++ task/Parser.cpp
@@ -58,19 +58,24 @@ void Parser::ExpressionTraversal(char* str)
 {
 	char s{ '\0' };
 	int len = strlen(str);
-	for (int i = 0; i < len; i++)
+
+	for (int i = 0; i < len; i++) // Проверка на корректность символов в строке.
 	{
 		if (
-			str[i] != '0' || str[i] != '1' || str[i] != '2' ||
-			str[i] != '3' || str[i] != '4' || str[i] != '5' ||
-			str[i] != '6' || str[i] != '7' || str[i] != '8' ||
-			str[i] != '9' ||
-			str[i] != '-' || str[i] != '*' || str[i] != '/' ||
-			str[i] != '^' || str[i] != '+'
+			str[i] != '0' && str[i] != '1' && str[i] != '2' &&
+			str[i] != '3' && str[i] != '4' && str[i] != '5' &&
+			str[i] != '6' && str[i] != '7' && str[i] != '8' &&
+			str[i] != '9' &&
+			str[i] != '-' && str[i] != '*' && str[i] != '/' &&
+			str[i] != '^' && str[i] != '+'
 			)
 		{
 			throw InvalidCharacterInExpression("Недопустимый символ в выражении"); // Сгенерировать исключение типа InvalidCharacterInExpression.
 		} // TODO: попробовать добавить в параметры str[i]
+	}
+
+	for (int i = 0; i < len; i++)
+	{
 		switch (str[i])
 		{
 		case '(':
